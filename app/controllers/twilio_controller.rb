b22034737cd65924b1797de9f0126f781e82886e
Greg Buckner <gbuckner23@gmail.com>
2013-11-07 21:42:27 -0800

    Added ability to text new tasks to my user account

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,9 +1,13 @@
 class TwilioController < ApplicationController
-  
+
   skip_before_filter :verify_authenticity_token
 
   def sms
-    render text: "I got your text! You said #{params["Body"]}", content_type: "text/plain"
+    task = Task.new
+    task.name = params["Body"]
+    task.user_id = User.find_by_phone_number(params["From"])
+    task.save!
+    render text: "Task successfully saved!", content_type: "text/plain"
   end
 
 end
