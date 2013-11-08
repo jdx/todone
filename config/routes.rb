5c50e804dbf82146117caa0a7c7f1a38b1468fb1
Greg Buckner <gbuckner23@gmail.com>
2013-11-07 21:05:44 -0800

    Authentication working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Todone::Application.routes.draw do
+  
+  get "login" => "sessions#new", as: "login"
+  post "login" => "sessions#create"
+  get "logout" => "sessions#destroy", as: "logout"
+
+  resources :users
   resources :tasks
   root to: 'tasks#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131106032546) do
+ActiveRecord::Schema.define(version: 20131108032301) do
 
   create_table "tasks", force: true do |t|
     t.integer  "list_id"
     t.string   "name"
     t.date     "due_date"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "phone_number"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
